7873cbd02868e4aa477b8375a975ead2a3a3c08a
Pavel Zhukov <pavel@zhukoff.net>
2012-08-21 18:56:13 +0400

    Move parent tag to settings

--- create_tag.py
+++ create_tag.py
@@ -5,7 +5,7 @@ import sys
 import xml.dom.minidom
 from xml.dom.minidom import getDOMImplementation
 from rhev_connection import *
-import libxml2
+import rhev_settings
 
 def create_tag_xml(tagname,parenttag):
     dom = getDOMImplementation()
@@ -20,21 +20,7 @@ def create_tag_xml(tagname,parenttag):
     textNode = document.createTextNode(tagname)
     firstElement.appendChild(textNode)
     return document.toxml()
-def get_tag_uuid(tagname):
-    tags = rhev_get("/api/tags")
-    doc = libxml2.parseDoc(tags)
-    ctxt = doc.xpathNewContext()
-    res = ctxt.xpathEval("/tags/tag[name[position()=1]= '" + tagname + "']")
-    for i in res:
-        return i.prop("id")
 
-def get_cluster_href(cluster_name):
-    clusters = rhev_get("/api/clusters")
-    doc = libxml2.parseDoc(clusters)
-    ctxt = doc.xpathNewContext()
-    res = ctxt.xpathEval("/clusters/cluster[name [position()=1]= '"+ cluster_name + "']")
-    for i in res:
-        return i.prop('href')
 
 def create_tag(tagname,parenttag):
     print rhev_post("/api/tags", create_tag_xml(tagname,parenttag))
@@ -42,6 +28,6 @@ def create_tag(tagname,parenttag):
 if __name__ == '__main__':
     if len(sys.argv) != 2: sys.exit(1)
     tagname = sys.argv[1]
-    create_tag(tagname,get_tag_uuid("projects"))
+    create_tag(tagname,get_tag_data(rhev_settings.TAG,"id"))
 
 
